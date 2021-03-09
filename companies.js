063cb5d67a5d37febc7b70c869207a778570c0c3
Alexandre-Rosier <alex.rosier1405@gmail.com>
2021-03-09 16:35:08 +0100

    first commit create API

---
+++ companies.js
@@ -0,0 +1,25 @@
+const companies = [
+    {
+        id: 0,
+        name: "NewDealFrance",
+        director: "Alban Dumont",
+        location: "Nantes",
+        picture:"./images/image-NDF.jpg"
+      },
+    {
+        id: 1,
+        name: "Airbus",
+        director: "Gérald Lignon",
+        location: "Montoir-de-bretagne",
+        picture:"./images/image-airbus.jpg"
+    },
+      {
+        id: 2,
+        name: "WildCodeSchool",
+        director: "Anna Stépanoff",
+        location: "Europe",
+        picture:"./images/image-wildcodeschool.png"
+      }
+    ];
+    
+    module.exports = companies;
---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 3002;
+const companies = require("./companies");
+
+app.get("/companies", (req, res) => {
+    res.status(200).json(companies);
+  });
+
+app.get("/companies/search/:name", (req, res) => {
+    const search = companies.filter((company) => company.name === (req.params.name));
+    search.length > 0
+    ? res.status(200).json(search)
+    : res.status(404).send("Company not found.. Warning => Check the writing of the company");
+   
+  });
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+  });
