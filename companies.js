6c15b80d3c9cda5b23d8a97fb1778456bc714ea1
Alexandre-Rosier <alex.rosier1405@gmail.com>
2021-03-11 09:55:08 +0100

    lien front et back

--- companies.js
+++ companies.js
@@ -4,21 +4,21 @@ const companies = [
         name: "NewDealFrance",
         director: "Alban Dumont",
         location: "Nantes",
-        picture:"./images/image-NDF.jpg"
+        picture:"images/image-NDF.jpg"
       },
     {
         id: 1,
         name: "Airbus",
         director: "Gérald Lignon",
         location: "Montoir-de-bretagne",
-        picture:"./images/image-airbus.jpg"
+        picture:"images/image-airbus.jpg"
     },
       {
         id: 2,
         name: "WildCodeSchool",
         director: "Anna Stépanoff",
         location: "Europe",
-        picture:"./images/image-wildcodeschool.png"
+        picture:"images/image-wildcodeschool.png"
       }
     ];
     
--- index.js
+++ index.js
@@ -3,17 +3,20 @@ const app = express();
 const port = 3001;
 const companies = require("./companies");
 
-// app.get("/companies", (req, res) => {
-//     res.status(200).json(companies);
-//   });
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
-// app.get("/companies/search/:name", (req, res) => {
-//     const search = companies.filter((company) => company.name === (req.params.name));
-//     search.length > 0
-//     ? res.status(200).json(search)
-//     : res.status(404).send("Company not found.. Warning => Check the writing of the company");
+app.get("/companies", (req, res) => {
+    res.status(200).json(companies);
+  });
+
+app.get("/companies/search/:name", (req, res) => {
+    const search = companies.filter((company) => company.name === (req.params.name));
+    search.length > 0
+    ? res.status(200).json(search)
+    : res.status(404).send("Company not found.. Warning => Check the writing of the company");
    
-//   });
+  });
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
